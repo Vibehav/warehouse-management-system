154f5d17fe14fe80bd3f29167440e78639c057bc
vibehav <vibehav@protonmail.com>
2026-08-24 20:50:52 +0530

    validation checks in DTO. Positive and Not null.

--- src/main/java/com/project/wms/inventory/controller/InboundController.java
+++ src/main/java/com/project/wms/inventory/controller/InboundController.java
@@ -5,6 +5,7 @@ import com.project.wms.inventory.dto.ConfirmPutawayRequestDto;
 import com.project.wms.inventory.dto.PutawayResponseDto;
 import com.project.wms.inventory.dto.ReceiveRequestDto;
 import com.project.wms.inventory.facade.InboundPutawayFacade;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -20,7 +21,7 @@ public class InboundController {
 
     @PostMapping("/receive")
     @PreAuthorize("hasAuthority('INBOUND_RECEIVE')")
-    public ResponseEntity<PutawayResponseDto> receive(@RequestBody ReceiveRequestDto request) {
+    public ResponseEntity<PutawayResponseDto> receive(@Valid @RequestBody ReceiveRequestDto request) {
         InboundPutawayFacade.PutawayResult result = putawayFacade.receive(
                 request.skuId(), request.supplierId(), request.warehouseId(),
                 request.quantity(), request.batchNo(), request.expiryDate()
@@ -31,7 +32,7 @@ public class InboundController {
 
     @PostMapping("/confirm")
     @PreAuthorize("hasAuthority('INBOUND_RECEIVE')")
-    public ResponseEntity<InventoryLot> confirm(@RequestBody ConfirmPutawayRequestDto request) {
+    public ResponseEntity<InventoryLot> confirm(@Valid @RequestBody ConfirmPutawayRequestDto request) {
         InventoryLot lot = putawayFacade.confirmPutaway(request.lotId(), request.scannedLocationCode());
         return ResponseEntity.ok(lot);
     }

b142ce211c7b33e64bbda67605f5e1939121da88
vibehav <vibehav@protonmail.com>
2026-08-24 16:41:46 +0530

    wms app

--- src/main/java/com/project/wms/WmsApplication.java
+++ src/main/java/com/project/wms/WmsApplication.java
@@ -1,5 +1,6 @@
 package com.project.wms;
 
+import com.project.wms.auth.config.CookieProperties;
 import com.project.wms.auth.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,7 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableConfigurationProperties(JwtProperties.class)
+@EnableConfigurationProperties({JwtProperties.class, CookieProperties.class})
 @EnableScheduling
 public class WmsApplication {
 
